c893f76b17cf0e500e0f9fdafe8223ffe83fea51
Aleksandrs221RDB141 <124677002+Aleksandrs221RDB141@users.noreply.github.com>
2023-04-16 16:23:48 +0000

    trying for first time

--- hash_substring.py
+++ hash_substring.py
@@ -3,8 +3,14 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
+    text = input()
+    if "F" in text :
+        with open(f"test/06") as f:
+            patt = f.readline()
+            text = f.readline()
+    elif "I" in text:
+        patt = input()
+        text = input()  
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -13,7 +19,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (patt.rstrip(), patt.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +27,33 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    pnlg = len(pattern)
+    txlg = len(text)
+    hasht=0
+    hashp=0
+    chs = 56
+    q =11
+    bup=1
+    rez = []
+    for i in range(pnln-1):
+        bup = (h*chs)%q
+    for i in range(pnln):
+        hashp=(hashp*chs+ord(text[i]))%q
+        hasht=(hasht*chs+ord(text[i]))%q
+    for i in range(txln-pnln+1):
+        if hashp == hasht:
+            for j in range(pnln):
+                if text[i+j] == pattern[j]:
+                    rez.append(i)
+                else:
+                    break
+        if i < txlg-pnlg:
+            hasht = (chs *(hasht - ord (text[i])*bup)+ord (text[i+pnlg])) % q
+            if hasht < 0:
+                t=t+q
 
     # and return an iterable variable
-    return [0]
+    return arr
 
 
 # this part launches the functions
